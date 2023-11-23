881b83935f25c86c9e7a1feb56ea6e023ea4563f
lucastack <lucs.suarez@gmail.com>
2023-11-23 19:53:34 -0300

    update .zip file path

--- terraform/compute_engine/assets.tf
+++ terraform/compute_engine/assets.tf
@@ -1,12 +1,12 @@
 resource "google_storage_bucket_object" "assets" {
   name       = format("%s-%s.zip", "assets", data.archive_file.source_zip.output_md5)
-  source     = format("%ssource.zip", "../src/")
+  source     = "../../source.zip"
   bucket     = var.bucket_name
   depends_on = [google_storage_bucket.gcs]
 }
 
 data "archive_file" "source_zip" {
   type        = "zip"
-  source_dir  = "../src/"
-  output_path = format("%ssource.zip", "../src/")
+  source_dir  = "../../src/"
+  output_path = "../../source.zip"
 }
